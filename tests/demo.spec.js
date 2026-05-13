fcb4ebcf12947cafcb0c1a0ba973335652ffb6df
vinithapavini279-cell <vinithapavini279@gmail.com>
2026-05-13 16:15:02 +0530

    Update demo.spec.js

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -1,25 +1,25 @@
-//const{test,expect}=require('@playwright/test'); // common Js module
-//except -> validation purpose
-// '@playwright/test -> package
-import{test,expect}from'@playwright/test'; //ES module
-test('launch url', async({page})=>{
-    await page.goto("https://www.facebook.com/");
-    await page.waitForTimeout(3000);
-    await page.goto("https://www.instagram.com/");
-    await page.waitForTimeout(2000);
-    await page.goback();
-    await page.goForward();
-    await page.reload();
-})
+// //const{test,expect}=require('@playwright/test'); // common Js module
+// //except -> validation purpose
+// // '@playwright/test -> package
+// import{test,expect}from'@playwright/test'; //ES module
+// test('launch url', async({page})=>{
+//     await page.goto("https://www.facebook.com/");
+//     await page.waitForTimeout(3000);
+//     await page.goto("https://www.instagram.com/");
+//     await page.waitForTimeout(2000);
+//     await page.goback();
+//     await page.goForward();
+//     await page.reload();
+// })
 
-//default -headless
+// //default -headless
 
-// cmd or config
+// // cmd or config
 
-// command -> npx playwright test --headed --project=webkit
+// // command -> npx playwright test --headed --project=webkit
 
-/*test('amazon', async({page})=>{
-    await page.goto("https://www.instagram.com/");
-})*/
+// /*test('amazon', async({page})=>{
+//     await page.goto("https://www.instagram.com/");
+// })*/
 
-// async -> to handle asyncronisation/ parallel 
+// // async -> to handle asyncronisation/ parallel 
