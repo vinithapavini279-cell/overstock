bd6a55ae7bac77ef6e55ea7d5a1fac905e35d75e
vinithapavini279-cell <vinithapavini279@gmail.com>
2026-05-13 16:16:32 +0530

    Update script.spec.js

--- tests/script.spec.js
+++ tests/script.spec.js
@@ -1,6 +1,6 @@
-import{test} from '@playwright/test';
-test('product select using if condition',async({page})=>{
-    await page.goto('https://www.amazon.in');
-    await page.locator('input#twotabsearchtextbox').fill('sarees');
-    await page.keyboard.press('enter');
-})
+// import{test} from '@playwright/test';
+// test('product select using if condition',async({page})=>{
+//     await page.goto('https://www.amazon.in');
+//     await page.locator('input#twotabsearchtextbox').fill('sarees');
+//     await page.keyboard.press('enter');
+// })
