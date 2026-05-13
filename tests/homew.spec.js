5ba6d7970a5e00a01511de03c3bc579a658992c2
vinithapavini279-cell <vinithapavini279@gmail.com>
2026-05-13 16:15:32 +0530

    Update homew.spec.js

--- tests/homew.spec.js
+++ tests/homew.spec.js
@@ -1,11 +1,11 @@
-import{test} from '@playwright/test';
-test('Add ptoducts to cart', async({page})=>{
-    await page.goto('https://www.myntra.com/boy-tshirts');
-    //await page.locator('(//div[@class="desktop-navLink"])[3]').click();
-    const total_Product=await page.locator('//div[@class="product-imageSliderContainer"]').count();
-    await page.waitForTimeout(2000);
-    console.log(total_Product);
-    const price=await page.locator('//span[@class="product-discountedPrice"]');
-    const pri=await price.allTextContents();
-    console.log(pri);
-})
+// import{test} from '@playwright/test';
+// test('Add ptoducts to cart', async({page})=>{
+//     await page.goto('https://www.myntra.com/boy-tshirts');
+//     //await page.locator('(//div[@class="desktop-navLink"])[3]').click();
+//     const total_Product=await page.locator('//div[@class="product-imageSliderContainer"]').count();
+//     await page.waitForTimeout(2000);
+//     console.log(total_Product);
+//     const price=await page.locator('//span[@class="product-discountedPrice"]');
+//     const pri=await price.allTextContents();
+//     console.log(pri);
+// })
